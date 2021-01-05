f866fef1d8daaa13f0f382b8bbab102bcb702b18
Prabhjeet <iprabhjeet9@gmail.com>
2021-01-05 21:50:44 +0530

    Enhanced Generic and MyList

--- src/Excercise/MyList.scala
+++ src/Excercise/MyList.scala
@@ -1,29 +1,39 @@
 package Excercise
 
-abstract class MyList{
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A]{
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B>:A](element: B): MyList[B]
+  def printElement: String
+  override def toString: String = "["+printElement+"]"
 
 }
 
-object Empty extends MyList {
-  def head: Int= throw new NoSuchElementException
-  def tail: MyList= throw new NoSuchElementException
+object Empty extends MyList[Nothing]{
+  def head: Nothing= throw new NoSuchElementException
+  def tail: MyList[Nothing]= throw new NoSuchElementException
   def isEmpty: Boolean= true
-  def add(element: Int): MyList= new Cons(element,Empty)
+  def add[B>:Nothing](element: B): MyList[B]= new Cons(element,Empty)
+  override def printElement: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList{
-  def head: Int= h
-  def tail: MyList= t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A]{
+  def head: A= h
+  def tail: MyList[A]= t
   def isEmpty: Boolean= false
-  def add(element: Int): MyList= new Cons(element,this)
+  def add[B>:A](element: B): MyList[B]= new Cons(element,this)
+
+  override def printElement: String =
+    if(t.isEmpty) ""+h
+    else h+" "+t.printElement
 }
 
 object Run extends App{
   val myList=new Cons(1,new Cons(2,new Cons(3,Empty)))
-  println(myList.tail.tail.head)
-  print(myList.tail)
+  val stringList= new Cons("one",new Cons("two",new Cons("three",Empty)))
+  println(myList.toString)
+  println(stringList.toString)
+
+
 }
--- src/lectures/part2/Generics.scala
+++ src/lectures/part2/Generics.scala
@@ -2,13 +2,37 @@ package lectures.part2
 
 object Generics extends App{
 
-  class MyList[A]
+  class MyList[+A]{
+    def add[B>:A](element: B): MyList[B]= ???
+  }
   val myList1=new MyList[Int]
   val myList2=new MyList[String]
 
   class Animal
   class Dog extends Animal
   class Cat extends Animal
+
+  def printAnimal(myList: MyList[Animal]): Unit={
+    println(myList)
+  }
+  val mylist=new MyList[Animal]
+  val myCat=new MyList[Cat]
+  printAnimal(mylist)
+  printAnimal(myCat)
+
+  class SecondList[-A]{
+    def add[A](element: A): Unit= println("Adding ",element)
+  }
+
+  def printCat(secondList: SecondList[Cat])={
+    println(secondList)
+  }
+  val secondCat = new SecondList[Cat]
+  printCat(secondCat)
+  val secondDog = new SecondList[Animal]
+  printCat(secondDog)
+  secondCat.add(secondDog)
+
 
   class Covar[+A]
   val covar: Covar[Animal]=new Covar[Dog]
